852639f7330bca3e269c16f80b2931d44b207ce9
Mykhailo <ivchenkomykhailo@gmail.com>
2025-07-13 15:43:33 +0300

    Add the toast provider

--- src/investment-app-frontend/src/main.tsx
+++ src/investment-app-frontend/src/main.tsx
@@ -5,11 +5,13 @@ import FallBackPage from './components/FallbackPage';
 import { ErrorBoundary } from 'react-error-boundary';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import ToastProvider from './components/ToastProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ErrorBoundary fallbackRender={FallBackPage}>
     <Provider store={store}>
       <App />
+      <ToastProvider />
     </Provider>
   </ErrorBoundary>
 );
