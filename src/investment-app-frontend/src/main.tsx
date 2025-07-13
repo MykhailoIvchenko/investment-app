22605dbce63e33a35f27f884866bd5c2486d7f9e
Mykhailo <ivchenkomykhailo@gmail.com>
2025-07-13 15:57:29 +0300

    Add the identity kit provider

--- src/investment-app-frontend/src/main.tsx
+++ src/investment-app-frontend/src/main.tsx
@@ -6,12 +6,18 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import ToastProvider from './components/ToastProvider';
+// @ts-ignore
+import { IdentityKitProvider } from '@nfid/identitykit/react';
+import { Plug } from '@nfid/identitykit';
+import '@nfid/identitykit/react/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ErrorBoundary fallbackRender={FallBackPage}>
-    <Provider store={store}>
-      <App />
-      <ToastProvider />
-    </Provider>
+    <IdentityKitProvider signers={[Plug]}>
+      <Provider store={store}>
+        <App />
+        <ToastProvider />
+      </Provider>
+    </IdentityKitProvider>
   </ErrorBoundary>
 );
