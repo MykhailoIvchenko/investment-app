cd3cb4bed7d41a2f1f2811e47a649a4dc636d52b
Mykhailo <ivchenkomykhailo@gmail.com>
2025-07-13 15:39:58 +0300

    Add the redux provider

--- src/investment-app-frontend/src/main.tsx
+++ src/investment-app-frontend/src/main.tsx
@@ -3,9 +3,13 @@ import App from './App';
 import './styles/index.scss';
 import FallBackPage from './components/FallbackPage';
 import { ErrorBoundary } from 'react-error-boundary';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ErrorBoundary fallbackRender={FallBackPage}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ErrorBoundary>
 );
